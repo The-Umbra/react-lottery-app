4b4139ed44d3aa0e3a56147d9d17168e8d06a7da
Furkan <gami.furkan@gmail.com>
2023-01-11 17:47:53 +0300

    background changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,11 +3,13 @@ import Slider from './Slider'
 import './assets/css/body.css'
 import LotteryContainer from "./LotteryBody";
 
+
 const Body = () => {
     return <div className="root">
         <section className="f-sec">
             <Slider />
         </section>
+        <div className="brand-line"></div>
         <section className="S-sec">
             <h1>Active Raffle</h1>
             <LotteryContainer />
