8ca0bbb211ba907b660116b18105c264bc44cbc5
Furkan <gami.furkan@gmail.com>
2023-01-12 14:05:44 +0300

    New Images Added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,10 +3,13 @@ import Slider from './Slider'
 import './assets/css/body.css'
 import LotteryContainer from "./LotteryBody";
 
+import Revolver from "./assets/images/revolver.png"
+import Revolver1 from "./assets/images/revolver-1.png"
 
 const Body = () => {
     return <div className="root">
         <section className="f-sec">
+            <div className="f-head"><img src={Revolver} alt="" /> <h1>WANTED</h1> <img src={Revolver1} alt="" /></div>
             <Slider />
         </section>
         <div className="brand-line"></div>
